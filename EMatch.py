345559b77f05057741e6cf07980f8ae3a0a9e531
mstettler <michael.stettler@cin.uni-tuebingen.de>
2020-09-16 11:55:35 +0200

    started to solve EMatch

--- EMatch.py
+++ EMatch.py
@@ -26,6 +26,10 @@ class EMatch:
         :param dp: delta p (k, n)
         :return: e_match
         """
+        # reshape dp in case it comes as a 1D array
+        if len(np.shape(dp)) < 2:
+            dp = np.reshape(dp, (self.K, self.N))
+
 
         # diagonalize uk
         diag_uk = np.array([np.diag(uk) for uk in self.uk])  # using diag(self.uk) would result of getting only the diagonal elements
@@ -54,24 +58,46 @@ if __name__ == '__main__':
     n_n = 4
     tckf = np.random.rand(n_k, n_f)  # (k, f)
     uk = np.random.rand(n_k, n_n)
-    daf = np.random.rand(n_f, n_n)
+    da = np.random.rand(n_f, n_n)
     dp = np.random.rand(n_k, n_n)
 
     # control compute e_match
     ematch_ctrl = 0
     for f in range(n_f):
         for k in range(n_k):
-            norm = np.linalg.norm(dp[k] - np.diag(uk[k]) @ daf[f])
+            norm = np.linalg.norm(dp[k] - np.diag(uk[k]) @ da[f])
             ematch_ctrl += tckf[k, f] * norm**2
     ematch_ctrl /= n_f
     print("ematch_ctrl")
     print(ematch_ctrl)
 
     # compute e_match
-    e_match_fn = EMatch(tckf, uk, daf).get_eMatch()
+    e_match_fn = EMatch(tckf, uk, da).get_eMatch()
     ematch = e_match_fn(dp)
     print("ematch")
     print(ematch)
 
     assert ematch == ematch_ctrl
     print("ematch values are equal")
+    print()
+
+    print("----- Minimization ------")
+    print("try optimizer")
+    print("shape dp", np.shape(dp))
+    from scipy import optimize
+    opt = optimize.minimize(e_match_fn, dp, method="CG")
+    print(opt)
+
+    print("try solve")
+    from scipy.linalg import solve
+    b = []
+    for f in range(n_f):
+        for k in range(n_k):
+            b.append(np.diag(uk[k]) @ da[f])
+    # b = np.sum(b, axis=1)
+    print("shape b", np.shape(b))
+    a = np.diag(dp.flatten())
+    print("shape a", np.shape(a))
+    sol = solve(a, b)
+    print(sol)
+    print()
