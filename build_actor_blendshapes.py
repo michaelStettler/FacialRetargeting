added xyz to xzy modifier, corrected the normalization function

build_actor_blendshapes.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 
 from utils.load_data import load_training_frames
 from utils.compute_delta import compute_delta
+from utils.remove_neutral_blendshape import remove_neutral_blendshape
+from utils.normalize_positions import normalize_positions
+from utils.modify_axis import modify_axis
 from utils.re_order_delta import re_order_delta
 from utils.get_key_expressions import get_key_expressions
 from src.compute_corr_coef import compute_corr_coef
@@ -23,11 +26,11 @@ np.set_printoptions(precision=4, linewidth=200, suppress=True)
 
 # define parameters
 actor_recording_data_folder = 'D:/MoCap_Data/David/NewSession_labeled/'
-blendshape_mesh_list_name = "D:/Maya projects/DigitalLuise/scripts/mesh_name_list.npy"
+blendshape_mesh_list_name = "C:/Users/Michael/PycharmProjects/FacialRetargeting/data/mesh_name_list.npy"
 load_folder = 'data/'
 sparse_blendhsape_vertices_pos_name = "louise_to_David_markers_blendshape_vertices_pos_v3.npy"
 save_folder = 'data/'
-save_file_name = "David_based_Louise_personalized_blendshapes_v3_norm_Ematch.npy"
+save_file_name = "David_based_Louise_personalized_blendshapes.npy"
 neutral_pose_name = 'Louise_Neutral'
 ref_actor_pose = 'data/David_neutral_pose.npy'
 max_num_seq = None  # set to None if we want to use all the sequences
@@ -37,38 +40,31 @@ save = True
 # load data
 mesh_list = np.load(blendshape_mesh_list_name).astype(str)
 sk = np.load(os.path.join(load_folder, sparse_blendhsape_vertices_pos_name))  # sparse representation of the blendshapes (vk)
-# get Neutral blendhsape pose
-ref_index = None
-bs_index = []
-cleaned_mesh_list = []
-for m, mesh in enumerate(mesh_list):
-    if mesh == neutral_pose_name:
-        ref_index = m
-    else:
-        bs_index.append(m)
-        cleaned_mesh_list.append(mesh)
-if ref_index is None:
-    raise ValueError("No Neutral blendshape pose found!")
-ref_sk = sk[ref_index]
-print("min ref_sk", np.min(ref_sk))
-print("max ref_sk", np.max(ref_sk))
-# sk = sk / np.linalg.norm(sk)
-min_sk = np.amin(ref_sk)
-max_sk = np.amax(ref_sk)
-ref_sk -= min_sk
-ref_sk /= max_sk
-sk -= min_sk
-sk /= max_sk
-# print("np.linalg.norm(sk)", np.linalg.norm(sk))
-print("min sk", np.min(sk))
-print("max sk", np.max(sk))
+# get Neutral ref index and new cleaned mesh list
+cleaned_mesh_list, bs_index, ref_index = remove_neutral_blendshape(mesh_list, neutral_pose_name)
+
+# get neutral (reference) blendshape and normalize it
+ref_sk, min_sk, max_sk = normalize_positions(np.copy(sk[ref_index]), return_min=True, return_max=True)
+
+# normalize sk
+sk = normalize_positions(sk, min_pos=min_sk, max_pos=max_sk)
+
+# compute delta sparse blendshape
 delta_sk = compute_delta(sk[bs_index, :, :], ref_sk)
 
 # test if delta_sk has no none-unique entry
-test_unique = np.unique(ref_sk, axis=1)
-if np.shape(test_unique)[0] != np.shape(ref_sk)[0]:
+test_unique = np.unique(delta_sk, axis=1)
+if np.shape(test_unique)[0] != np.shape(delta_sk)[0]:
     raise ValueError("delta_sk contains non unique entry! Check your index dictionary to build the sparse blendshape "
                      "(maya_scripts.03_extract_blendshapes_pos_and_obj)")
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.plot_trisurf(ref_sk[:, 0], ref_sk[:, 1], ref_sk[:, 2])
+ax.set_title("Ref sk")
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
 
 # get actor animation
 template_labels = ['LeftBrow1', 'LeftBrow2', 'LeftBrow3', 'LeftBrow4', 'RightBrow1', 'RightBrow2', 'RightBrow3',
@@ -80,116 +76,126 @@ template_labels = ['LeftBrow1', 'LeftBrow2', 'LeftBrow3', 'LeftBrow4', 'RightBro
                  'Head3', 'Head4']
 
 ref_actor_pose = np.load(ref_actor_pose)
-# norm_ref = np.linalg.norm(ref_actor_pose)
-min_af = np.amin(ref_actor_pose)
-max_af = np.amax(ref_actor_pose)
-# ref_actor_pose /= norm_ref
-ref_actor_pose -= min_af
-ref_actor_pose /= max_af
-af = load_training_frames(actor_recording_data_folder,
-                          num_markers=45,
-                          template_labels=template_labels,
-                          max_num_seq=max_num_seq,
-                          down_sample_factor=5)
-# af /= norm_ref
-af -= min_af
-af /= max_af
-delta_af = compute_delta(af, ref_actor_pose)
-print("delta_af")
-print(delta_af[0])
-print()
-print("delta_sk")
-print(delta_sk[0])
 ref_actor_pose = ref_actor_pose[:-4, :]  # remove HEAD markers
-af = af[:, :-4, :]  # remove HEAD markers
-delta_af = delta_af[:, :-4, :]  # remove HEAD markers
-print("[data] Finished loading data")
-print("[data] Neutral Blendshape index:", ref_index)
-print("[data] shape ref_actor_pose", np.shape(ref_actor_pose))
-print("[data] shape af:", np.shape(af))
-print("[data] shape sk", np.shape(sk))
-print("[data] shape delta_sk", np.shape(delta_sk))
-print("[data] shape delta af:", np.shape(delta_af))
-print("[data] cleaned_mesh_list:", len(cleaned_mesh_list))
 
-# get dimensions
-K, M, n_dim = np.shape(delta_sk)
-F = np.shape(delta_af)[0]
-print("[data] num_blendshapes:", K)
-print("[data] num_markers:", M)
-print("[data] num_features (M*3):", M*n_dim)
-print("[data] num_frames", F)
-print()
+# normalize reference (neutral) actor positions
+ref_actor_pose, min_af, max_af = normalize_positions(ref_actor_pose, return_min=True, return_max=True)
+# modify axis from xyz to xzy to match the scatter blendshape axis orders
+ref_actor_pose = modify_axis(ref_actor_pose, order='xzy', inverse_z=True)
 
-# 1) Facial Motion Similarity
-# reorder delta blendshapes
-sorted_delta_sk, sorted_index = re_order_delta(delta_sk)
-sorted_mesh_list = np.array(cleaned_mesh_list)[sorted_index]
-print("[Pre-processing] shape sorted_delta_sk", np.shape(sorted_delta_sk))
-print("[Pre-processing] len sorted_mesh_list", len(sorted_mesh_list))
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.scatter(ref_actor_pose[:, 0], ref_actor_pose[:, 1], ref_actor_pose[:, 2])
+ax.set_title("A0")
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+plt.show()
 
-# measure similarity between character blendshapes and actor's capture performance
-ckf = compute_corr_coef(np.reshape(delta_af, (np.shape(delta_af)[0], -1)),
-                        np.reshape(sorted_delta_sk, (np.shape(sorted_delta_sk)[0], -1)))
-if do_plot:
-    plot_similarities(ckf, "Fig. 7: Motion space similarity")
-
-# contrast enhancement
-tk = compute_trust_values(np.reshape(sorted_delta_sk, (np.shape(sorted_delta_sk)[0], -1)), do_plot=do_plot)
-tilda_ckf = compute_tilda_corr_coef(ckf, tk)
-print("[Pre-processing] shape ckf", np.shape(ckf))
-print("[Pre-processing] shape tk", np.shape(tk))
-print("[Pre-processing] shape tilda_ckf", np.shape(tilda_ckf))
-print()
-
-# 2) Key Expression Extraction
-key_expressions_idx = get_key_expressions(tilda_ckf, ksize=3, theta=2, do_plot=do_plot)
-print("[Key Expr. Extract.] shape key_expressions", np.shape(key_expressions_idx))
-F = len(key_expressions_idx)
-print("[Key Expr. Extract.] Keep", F, "frames")
-delta_af = delta_af[key_expressions_idx, :, :]
-tilda_ckf = tilda_ckf[:, key_expressions_idx]
-print("[Key Expr. Extract.] shape delta_af", np.shape(delta_af))
-print("[Key Expr. Extract.] shape tilda_ckf", np.shape(tilda_ckf))
-print()
-
-# 3) Manifold Alignment
-# built soft max vector
-uk = get_soft_mask(sorted_delta_sk)
-print("[SoftMax] shape uk", np.shape(uk))
-print()
-
-# 4) Geometric Constraint
-# build initial guess blendshape using RBF wrap (in delta space)
-delta_gk = get_initial_actor_blendshapes(ref_sk, ref_actor_pose, sorted_delta_sk)
-print("[RBF Wrap] shape delta_gk", np.shape(delta_gk))
-print()
-
-# 5) build personalized actor-specific blendshapes (delta_p)
-# rehsape to match required dimensions
-delta_af = np.reshape(delta_af, (F, M*n_dim))
-sorted_delta_sk = np.reshape(sorted_delta_sk, (K, M*n_dim))
-# print control of all shapes
-print("[dp] shape tilda_ckf:", np.shape(tilda_ckf))
-print("[dp] shape uk:", np.shape(uk))
-print("[dp] shape delta_af:", np.shape(delta_af))
-print("[dp] shape delta_gk:", np.shape(delta_gk))
-print("[dp] shape delta_sk", np.shape(sorted_delta_sk))
-# declare E_Align
-e_align = EAlign(tilda_ckf, uk, delta_af, delta_gk, sorted_delta_sk)
-# compute personalized actor-specific blendshapes
-start = time.time()
-delta_p = e_align.compute_actor_specific_blendshapes(vectorized=False)
-print("[dp] Solved in:", time.time() - start)
-print("[dp] shape delta_p", np.shape(delta_p))
-print()
-
-# 6) save delta_p ans sorted_mesh_list
-if save:
-    np.save(save_folder + save_file_name, delta_p)
-    np.save(save_folder + 'sorted_mesh_name_list', sorted_mesh_list)
-    print("[save] saved delta_pk (actor specifik blendshapes), shape:", np.shape(delta_p))
-    print("[save] saved sorted_mesh_list, shape:", np.shape(delta_p))
-
-if do_plot:
-    plt.show()
+# af = load_training_frames(actor_recording_data_folder,
+#                           num_markers=45,
+#                           template_labels=template_labels,
+#                           max_num_seq=max_num_seq,
+#                           down_sample_factor=5)
+# af = af[:, :-4, :]  # remove HEAD markers
+# print("shape af", np.shape(af))
+# # af /= norm_ref
+# af -= min_af
+# af /= max_af
+# delta_af = compute_delta(af, ref_actor_pose)
+# print("delta_af")
+# print(delta_af[0])
+# print()
+# print("delta_sk")
+# print(delta_sk[0])
+# delta_af = delta_af[:, :-4, :]  # remove HEAD markers
+# print("[data] Finished loading data")
+# print("[data] Neutral Blendshape index:", ref_index)
+# print("[data] shape ref_actor_pose", np.shape(ref_actor_pose))
+# print("[data] shape af:", np.shape(af))
+# print("[data] shape sk", np.shape(sk))
+# print("[data] shape delta_sk", np.shape(delta_sk))
+# print("[data] shape delta af:", np.shape(delta_af))
+# print("[data] cleaned_mesh_list:", len(cleaned_mesh_list))
+#
+# # get dimensions
+# K, M, n_dim = np.shape(delta_sk)
+# F = np.shape(delta_af)[0]
+# print("[data] num_blendshapes:", K)
+# print("[data] num_markers:", M)
+# print("[data] num_features (M*3):", M*n_dim)
+# print("[data] num_frames", F)
+# print()
+#
+# # 1) Facial Motion Similarity
+# # reorder delta blendshapes
+# sorted_delta_sk, sorted_index = re_order_delta(delta_sk)
+# sorted_mesh_list = np.array(cleaned_mesh_list)[sorted_index]
+# print("[Pre-processing] shape sorted_delta_sk", np.shape(sorted_delta_sk))
+# print("[Pre-processing] len sorted_mesh_list", len(sorted_mesh_list))
+#
+# # measure similarity between character blendshapes and actor's capture performance
+# ckf = compute_corr_coef(np.reshape(delta_af, (np.shape(delta_af)[0], -1)),
+#                         np.reshape(sorted_delta_sk, (np.shape(sorted_delta_sk)[0], -1)))
+# if do_plot:
+#     plot_similarities(ckf, "Fig. 7: Motion space similarity")
+#
+# # contrast enhancement
+# tk = compute_trust_values(np.reshape(sorted_delta_sk, (np.shape(sorted_delta_sk)[0], -1)), do_plot=do_plot)
+# tilda_ckf = compute_tilda_corr_coef(ckf, tk)
+# print("[Pre-processing] shape ckf", np.shape(ckf))
+# print("[Pre-processing] shape tk", np.shape(tk))
+# print("[Pre-processing] shape tilda_ckf", np.shape(tilda_ckf))
+# print()
+#
+# # 2) Key Expression Extraction
+# key_expressions_idx = get_key_expressions(tilda_ckf, ksize=3, theta=2, do_plot=do_plot)
+# print("[Key Expr. Extract.] shape key_expressions", np.shape(key_expressions_idx))
+# F = len(key_expressions_idx)
+# print("[Key Expr. Extract.] Keep", F, "frames")
+# delta_af = delta_af[key_expressions_idx, :, :]
+# tilda_ckf = tilda_ckf[:, key_expressions_idx]
+# print("[Key Expr. Extract.] shape delta_af", np.shape(delta_af))
+# print("[Key Expr. Extract.] shape tilda_ckf", np.shape(tilda_ckf))
+# print()
+#
+# # 3) Manifold Alignment
+# # built soft max vector
+# uk = get_soft_mask(sorted_delta_sk)
+# print("[SoftMax] shape uk", np.shape(uk))
+# print()
+#
+# # 4) Geometric Constraint
+# # build initial guess blendshape using RBF wrap (in delta space)
+# delta_gk = get_initial_actor_blendshapes(ref_sk, ref_actor_pose, sorted_delta_sk)
+# print("[RBF Wrap] shape delta_gk", np.shape(delta_gk))
+# print()
+#
+# # 5) build personalized actor-specific blendshapes (delta_p)
+# # rehsape to match required dimensions
+# delta_af = np.reshape(delta_af, (F, M*n_dim))
+# sorted_delta_sk = np.reshape(sorted_delta_sk, (K, M*n_dim))
+# # print control of all shapes
+# print("[dp] shape tilda_ckf:", np.shape(tilda_ckf))
+# print("[dp] shape uk:", np.shape(uk))
+# print("[dp] shape delta_af:", np.shape(delta_af))
+# print("[dp] shape delta_gk:", np.shape(delta_gk))
+# print("[dp] shape delta_sk", np.shape(sorted_delta_sk))
+# # declare E_Align
+# e_align = EAlign(tilda_ckf, uk, delta_af, delta_gk, sorted_delta_sk)
+# # compute personalized actor-specific blendshapes
+# start = time.time()
+# delta_p = e_align.compute_actor_specific_blendshapes(vectorized=False)
+# print("[dp] Solved in:", time.time() - start)
+# print("[dp] shape delta_p", np.shape(delta_p))
+# print()
+#
+# # 6) save delta_p ans sorted_mesh_list
+# if save:
+#     np.save(save_folder + save_file_name, delta_p)
+#     np.save(save_folder + 'sorted_mesh_name_list', sorted_mesh_list)
+#     print("[save] saved delta_pk (actor specifik blendshapes), shape:", np.shape(delta_p))
+#     print("[save] saved sorted_mesh_list, shape:", np.shape(delta_p))
+#
+# if do_plot:
+#     plt.show()

retarget.py
@@ -3,91 +3,113 @@ import numpy as np
 from tqdm import tqdm
 from scipy.linalg import solve
 
+import multiprocessing as mp
+from functools import partial
+
 from utils.load_data import load_training_seq
 from utils.compute_delta import compute_delta
 from src.ERetarget import ERetarget
 
-#### missing: get blendshapes and marker data from maya here
 
-# define parameters
-ref_actor_pose = 'data/David_neutral_pose.npy'
-load_folder = "data/"
-delta_p_name = "David_based_Louise_personalized_blendshapes_v3_norm_Ematch.npy"
-LdV_name = "LdV_louise.npy"
-load_sequence_folder = "D:/MoCap_Data/David/NewSession_labeled/"
-# sequence_name = "AngerTrail05.c3d"
-sequence_name = "HappyTrail01.c3d"
-# sequence_name = "FearTrail03.c3d"
-num_markers = 45
-save_folder = "data/"
-save_name = "weights_David2Louise_retarget_Happy_500_L1_v3_norm_Ematch"
-# save_name = "weights_David2Louise_retarget_FearTrail"
-use_L2 = False
-
-# ----------------------- data -------------------------
-# load data
-delta_p = np.load(os.path.join(load_folder, delta_p_name))
-LdV = np.load(os.path.join(load_folder, LdV_name))
-# LdV /= np.linalg.norm(LdV)  # todo normalize dV and not LdV?
-print("max ldv", np.amax(LdV))
-# load sequence to retarget
-ref_actor_pose = np.load(ref_actor_pose)
-# norm_ref = np.linalg.norm(ref_actor_pose)
-min_af = np.amin(ref_actor_pose)
-max_af = np.amax(ref_actor_pose)
-# ref_actor_pose /= norm_ref
-ref_actor_pose -= min_af
-ref_actor_pose /= max_af
-af = load_training_seq(load_sequence_folder, sequence_name, num_markers)
-# af /= norm_ref
-af -= min_af
-af /= max_af
-delta_af = compute_delta(af, ref_actor_pose)
-ref_actor_pose = ref_actor_pose[:-4, :]  # remove HEAD markers
-af = af[:, :-4, :]  # remove HEAD markers
-delta_af = delta_af[:, :-4, :]  # remove HEAD markers
-delta_af = np.reshape(delta_af, (np.shape(delta_af)[0], -1))
-
-print("[data] Finish loading data")
-print("[data] shape delta_p", np.shape(delta_p))
-print("[data] shape LdV", np.shape(LdV))
-print("[data] shape delta_af", np.shape(delta_af))
-num_frames = np.shape(delta_af)[0]
-num_blendshapes = np.shape(delta_p)[0]
-num_markers = np.shape(delta_p)[1] / 3
-print("[data] num frames:", num_frames)
-print("[data] num blendshapes:", num_blendshapes)
-print("[data] num_markers:", num_markers)
-print()
-
-# ----------------------- ERetarget -------------------------
-eRetarget = ERetarget(delta_p, LdV)
-
-weights = []
-for i in tqdm(range(4000, 4500)):
-# for i in tqdm(range(1589, 1590)):
-# for i in tqdm(range(num_frames)):
+def get_w(i, eRetarget, delta_af, use_L2):
     eRetarget.set_af(delta_af[i])
     A, b = eRetarget.get_dERetarget(L2=use_L2)
-    w = solve(A, b)
-    weights.append(w)
+    return solve(A, b)
 
-print("[Retarget] shape weights", np.shape(weights))
 
-# normalize weights
-weights = np.array(weights)
-print(weights[:, 0])
-max_weights = np.amax(weights)
-min_weights = np.amin(weights)
-max_index = np.argmax(weights)
-min_index = np.argmin(weights)
-print("max weights", max_weights, "at", max_index)
-print("min weights", min_weights, "at", min_index)
-# weights /= np.amax(weights)
-# save
-np.save(os.path.join(save_folder, save_name), weights)
-print("weights save as:", os.path.join(save_folder, save_name))
-print("max weights", np.amax(weights), "at", max_index)
-print("min weights", np.amin(weights), "at", min_index)
+if __name__ == '__main__':
+    pool = mp.Pool(processes=4)
+    #### missing: get blendshapes and marker data from maya here
+
+    # define parameters
+    ref_actor_pose = 'data/David_neutral_pose.npy'
+    load_folder = "data/"
+    delta_p_name = "David_based_Louise_personalized_blendshapes_v3_norm_EMatch.npy"
+    LdV_name = "LdV_louise.npy"
+    load_sequence_folder = "D:/MoCap_Data/David/NewSession_labeled/"
+    # sequence_name = "AngerTrail05.c3d"
+    sequence_name = "HappyTrail01.c3d"
+    # sequence_name = "FearTrail03.c3d"
+    # sequence_name = "NeutralTrail14.c3d"
+    num_markers = 45
+    save_folder = "data/"
+    save_name = "weights_David2Louise_retarget_Happy_15000_L1_v3_norm_EMatch"
+    # save_name = "weights_David2Louise_retarget_FearTrail"
+    use_L2 = False
+
+    # ----------------------- data -------------------------
+    # load data
+    delta_p = np.load(os.path.join(load_folder, delta_p_name))
+    print("max delta_p", np.amax(delta_p))
+    LdV = np.load(os.path.join(load_folder, LdV_name))
+    # LdV /= np.linalg.norm(LdV)  # todo normalize dV and not LdV?
+    print("max ldv", np.amax(LdV))
+    # load sequence to retarget
+    ref_actor_pose = np.load(ref_actor_pose)
+    # norm_ref = np.linalg.norm(ref_actor_pose)
+    min_af = np.amin(ref_actor_pose)
+    max_af = np.amax(ref_actor_pose)
+    # ref_actor_pose /= norm_ref
+    ref_actor_pose -= min_af
+    ref_actor_pose /= max_af
+    af = load_training_seq(load_sequence_folder, sequence_name, num_markers)
+    # af /= norm_ref
+    af -= min_af
+    af /= max_af
+    delta_af = compute_delta(af, ref_actor_pose)
+    print("delta_af")
+    print(delta_af[0])
+    ref_actor_pose = ref_actor_pose[:-4, :]  # remove HEAD markers
+    af = af[:, :-4, :]  # remove HEAD markers
+    delta_af = delta_af[:, :-4, :]  # remove HEAD markers
+    delta_af = np.reshape(delta_af, (np.shape(delta_af)[0], -1))
+
+    print("[data] Finish loading data")
+    print("[data] shape delta_p", np.shape(delta_p))
+    print("[data] shape LdV", np.shape(LdV))
+    print("[data] shape delta_af", np.shape(delta_af))
+    num_frames = np.shape(delta_af)[0]
+    num_blendshapes = np.shape(delta_p)[0]
+    num_markers = np.shape(delta_p)[1] / 3
+    print("[data] num frames:", num_frames)
+    print("[data] num blendshapes:", num_blendshapes)
+    print("[data] num_markers:", num_markers)
+    print()
+
+    # ----------------------- ERetarget -------------------------
+    eRetarget = ERetarget(delta_p, LdV)
+
+    # weights = []
+    # for i in tqdm(range(500)):
+    # # for i in tqdm(range(1589, 1590)):
+    # # for i in tqdm(range(num_frames)):
+    #     eRetarget.set_af(delta_af[i])
+    #     A, b = eRetarget.get_dERetarget(L2=use_L2)
+    #     w = solve(A, b)
+    #     weights.append(w)
+
+    # multiprocessing
+    p_get_w = partial(get_w, eRetarget=eRetarget, delta_af=delta_af, use_L2=use_L2)
+    weights = pool.map(p_get_w, tqdm(range(15000)))
+    pool.close()
+
+    print("[Retarget] shape weights", np.shape(weights))
+
+    # normalize weights
+    weights = np.array(weights)
+    print("shape weights", np.shape(weights))
+    print(weights[:, 0])
+    max_weights = np.amax(weights)
+    min_weights = np.amin(weights)
+    max_index = np.argmax(weights)
+    min_index = np.argmin(weights)
+    print("max weights", max_weights, "at", max_index)
+    print("min weights", min_weights, "at", min_index)
+    # weights /= np.amax(weights)
+    # save
+    np.save(os.path.join(save_folder, save_name), weights)
+    print("weights save as:", os.path.join(save_folder, save_name))
+    print("max weights", np.amax(weights), "at", max_index)
+    print("min weights", np.amin(weights), "at", min_index)
 
 